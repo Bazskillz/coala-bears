b1efafc6e4e02aef2c6e88d14054576edcfa4408
rojhat-hva <73133346+rojhat-hva@users.noreply.github.com>
2021-01-07 21:47:54 +0100

    Add files via upload

--- angularMakeHttpSecureBear.py
+++ angularMakeHttpSecureBear.py
@@ -7,11 +7,14 @@ class angularMakeHttpSecureBear(LocalBear):
             filename,
             file):
             for line in file:
-                if '"homepage": "http://'   in line:
+                if 'src="http://'   in line:
                     logging.debug("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!")
                     yield self.new_result("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!" + line + ".", file=filename)
                 elif '"url": "http://'   in line:
                     logging.debug("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!")
-                    yield self.new_result("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!" + line + ".", file=filename)		 
+                    yield self.new_result("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!" + line + ".", file=filename)
+                elif '"homepage": "http://'   in line:
+                    logging.debug("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!")
+                    yield self.new_result("Found HTTP:// in your code, Please make this referention HTTPS to make it more SECURE !!!" + line + ".", file=filename)
                 else:
                     logging.debug("Checking line")
